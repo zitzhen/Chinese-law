154a9bdb28fb0428979e6c8129edebe9a5392479
Oliver <149680880+Iamliuxiaozhen@users.noreply.github.com>
2025-05-22 01:00:50 +0800

    新增【法律详情页】仅限UI，仍在开发

---
+++ law/js/script.js
@@ -0,0 +1,17 @@
+const law_detail_container =document.getElementById("law_detail_container");
+const law_main_content = document.getElementById("law_main_content");
+const error_ms_1 = document.getElementById("error_ms_1");
+const error_windows =document.getElementById("error_windows")
+
+function new_error(ms){
+    law_detail_container.style.display = 'none';
+    error_windows.style.display = 'block';
+    error_ms_1.textContent = ms;
+}
+
+//获取URL参数
+const urlParams = new URLSearchParams(window.location.search);
+const filename = urlParams.get('name');
+if (!filename){
+    new_error("路径或参数不正确");
+}
