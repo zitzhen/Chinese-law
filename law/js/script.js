f340e953cf80a8fe6a1f60850a1587a56fc8b4b4
Oliver <149680880+Iamliuxiaozhen@users.noreply.github.com>
2025-05-24 07:46:10 +0800

    添加加载进度条样式和逻辑，优化用户体验

--- law/js/script.js
+++ law/js/script.js
@@ -2,13 +2,30 @@ const law_detail_container =document.getElementById("law_detail_container");
 const law_main_content = document.getElementById("law_main_content");
 const error_ms_1 = document.getElementById("error_ms_1");
 const error_windows =document.getElementById("error_windows");
-const name_url = document.getElementById("name_url")
+const name_url = document.getElementById("name_url");
+const Loading = document.getElementById("Loading");
 
 function new_error(ms){
     law_detail_container.style.display = 'none';
     error_windows.style.display = 'block';
     error_ms_1.textContent = ms;
+    Loading.style.display = 'none';
 }
+
+async function fetchMarkdown(filePath) {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const markdownContent = await response.text();
+    return markdownContent;
+  } catch (error) {
+    console.error('Error fetching markdown:', error);
+    return null;
+  }
+}
+
 
 //获取URL参数
 const urlParams = new URLSearchParams(window.location.search);
@@ -27,3 +44,4 @@ if(filename == 'index.html' || filename == 'name' || filename == ':name'){
 }
 console.log("文件名：", filename);
 name_url.textContent = filename;
+fetchMarkdown(`${filename}/README.md`)
