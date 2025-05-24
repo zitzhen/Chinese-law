b46b7cc83c874e7573946a9eac90990c36498cb4
Oliver <149680880+Iamliuxiaozhen@users.noreply.github.com>
2025-05-24 10:47:02 +0800

    完成法律详情页查询下载

--- law/js/script.js
+++ law/js/script.js
@@ -4,8 +4,17 @@ const error_ms_1 = document.getElementById("error_ms_1");
 const error_windows =document.getElementById("error_windows");
 const name_url = document.getElementById("name_url");
 const Loading = document.getElementById("Loading");
+const law_content = document.getElementById("law_content");
+const law_name =document.getElementById("law_name");
+const pdf_download=document.getElementById("pdf_download");
+const md_download =document.getElementById("md_download");
+const promulgate = document.getElementById("promulgate")
+if(!law_detail_container ||!law_main_content||!error_ms_1||!error_windows||!name_url||!Loading||!law_content||!law_name||!pdf_download||!md_download){
+    console.error("获取元素其一失败");
+}
 
 function new_error(ms){
+    console.error("触发错误");
     law_detail_container.style.display = 'none';
     error_windows.style.display = 'block';
     error_ms_1.textContent = ms;
@@ -27,22 +36,40 @@ async function fetchMarkdown(filePath) {
 }
 
 
-//获取URL参数
-const urlParams = new URLSearchParams(window.location.search);
-let filename = urlParams.get('name'); 
-if (!filename){
-    const pathMatch = window.location.pathname.match(/\/law\/([^\/\?]+)/);
-    if (pathMatch && pathMatch[1]) {
-        filename = decodeURIComponent(pathMatch[1]);
+async function main() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let filename = urlParams.get('name'); 
+    if (!filename){
+        const pathMatch = window.location.pathname.match(/\/law\/([^\/\?]+)/);
+        if (pathMatch && pathMatch[1]) {
+            filename = decodeURIComponent(pathMatch[1]);
+        }
+        else{
+            new_error("路径或参数不正确");
+            return;
+        }
     }
-    else{
+    if(filename == 'index.html' || filename == 'name' || filename == ':name'){
         new_error("路径或参数不正确");
+        return;
+    }
+    console.log("文件名：", filename);
+    name_url.textContent = filename;
+
+    const filemarkdown = await fetchMarkdown(`${filename}/README.md`);
+    console.log(filemarkdown);
+    if (filemarkdown) {
+        const filelaw = marked.parse(filemarkdown);
+        law_name.textContent = filename;
+        console.log(filelaw);
+        law_detail_container.style.display = 'flex';
+        law_content.innerHTML = filelaw;
+        pdf_download.href = window.location.hostname+`/${filename}/${filename}.pdf`
+        md_download.href = window.location.hostname+`/${filename}/README.md`
+        Loading.style.display = 'none';
+    } else {
+        new_error("未能加载文件内容");
     }
 }
-if(filename == 'index.html' || filename == 'name' || filename == ':name'){
-    new_error("路径或参数不正确");
-}
-console.log("文件名：", filename);
-name_url.textContent = filename;
-const filemarkdown =  fetchMarkdown(`${filename}/README.md`);
-console.log(filemarkdown);
+
+main();
