b5ef40479885d934e2e0967511e3b06abb188b83
Oliver <149680880+Iamliuxiaozhen@users.noreply.github.com>
2025-05-24 11:22:08 +0800

    完成状态栏的显示

--- law/js/script.js
+++ law/js/script.js
@@ -8,7 +8,10 @@ const law_content = document.getElementById("law_content");
 const law_name =document.getElementById("law_name");
 const pdf_download=document.getElementById("pdf_download");
 const md_download =document.getElementById("md_download");
-const promulgate = document.getElementById("promulgate")
+const promulgate = document.getElementById("promulgate");
+const implement = document.getElementById("implement");
+const level = document.getElementById("level");
+const state =document.getElementById("state");
 if(!law_detail_container ||!law_main_content||!error_ms_1||!error_windows||!name_url||!Loading||!law_content||!law_name||!pdf_download||!md_download){
     console.error("获取元素其一失败");
 }
@@ -35,6 +38,19 @@ async function fetchMarkdown(filePath) {
   }
 }
 
+async function fetch_json(filePath) {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error('Error fetching json:', error);
+    return null;
+  }
+}
 
 async function main() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -57,7 +73,9 @@ async function main() {
     name_url.textContent = filename;
 
     const filemarkdown = await fetchMarkdown(`${filename}/README.md`);
+    const jsondata = await fetch_json(`${filename}/information.json`);
     console.log(filemarkdown);
+    console.log(jsondata);
     if (filemarkdown) {
         const filelaw = marked.parse(filemarkdown);
         law_name.textContent = filename;
@@ -66,6 +84,10 @@ async function main() {
         law_content.innerHTML = filelaw;
         pdf_download.href = window.location.hostname+`/${filename}/${filename}.pdf`
         md_download.href = window.location.hostname+`/${filename}/README.md`
+        promulgate.textContent = "颁布日期："+jsondata.date_of_enactment;
+        implement.textContent = "实施日期："+jsondata.date_of_implementation;
+        level.textContent = "效力级别："+jsondata.potency_level;
+        state.textContent = "实施状态："+jsondata.Timeliness;
         Loading.style.display = 'none';
     } else {
         new_error("未能加载文件内容");
