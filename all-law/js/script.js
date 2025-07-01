3ac95b96947b6f9c12ce49665fa061991ebe87e3
oliver <xiaozhen49@outlook.com>
2025-07-02 01:05:43 +0800

    完成添加内容到前端的函数98%

--- all-law/js/script.js
+++ all-law/js/script.js
@@ -1,3 +1,15 @@
-await function fetchData(){
-    
+const legal_page = document.getElementById("legal_page");
+
+function add_law(name,date_of_enactment,reference_number,Timeliness){
+    legal_page.innerHTML +=`
+                    <tr>
+                        <td>
+                            <a href="#" class="law-name">${name}</a>
+                        </td>
+                        <td>${date_of_enactment}<td>
+                        <td>${date_of_implementation}</td>
+                        <td>${reference_number}</td>
+                        <td><span class="law-status status-valid">${Timeliness}</span></td>
+                        <td><a href="#">查看</a></td>
+                    </tr>`
 }
