244abe931fc571375a3321cb4a6333f19284826b
oliver <oliverxiaozhen@outlook.com>
2025-09-23 13:21:35 +0800

    修改App.vue与路由页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/index.vue'
+const routes = [
+    { 
+        path:'/',
+        name:'Home',
+        Comment:Home
+    },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
